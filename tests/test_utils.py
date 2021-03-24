825354dc1c28ae3bb51da34de45d1b63ef98e2fb
Efi Butovski <efibutov@gmail.com>
2021-03-24 15:14:40 +0200

    A few more tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -9,11 +9,11 @@ import utils
 @pytest.mark.parametrize(
     'test_value,expected',
     [
-        ('/hello/world', '_hello_world',),
-        ('*hello^world', '_hello^world'),
+        ('/hello|world', '_hello_world',),
+        ('*hello^world:', '_hello^world_'),
         ('*hello\\world', '_hello_world'),
         ('<hello?world>', '_hello_world_'),
-        ('[hello]world:"', '[hello]world__'),
+        ('[hello]world"', '[hello]world_'),
     ]
 )
 def test_get_proper_file_name_part(test_value, expected):
